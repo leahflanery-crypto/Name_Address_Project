0043b6791bc2a3d764caf4249865b889d929bdcd
leahflanery-crypto <leah.flanery@csuglobal.edu>
2025-09-14 19:10:18 -0500

    Add files via upload

---
+++ NameAddress.java
@@ -0,0 +1,14 @@
+package csc320;
+
+public class NameAddress {
+
+	public static void main(String[] args) {
+		System.out.println("Pipper");
+		System.out.println("Jones");
+		System.out.println("456 Jones Way Dr.");
+		System.out.println("Loveland");
+		System.out.println("88745");
+
+	}
+
+}
